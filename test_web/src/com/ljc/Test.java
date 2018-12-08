cfd8f17f18b34c3d691848b1a323c8fbcaf65288
ljc727 <ljc727@naver.com>
2018-12-08 20:51:46 +0900

    Update Test.java

--- test_web/src/com/ljc/Test.java
+++ test_web/src/com/ljc/Test.java
@@ -2,8 +2,6 @@ package com.ljc;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(2222);
-		System.out.println("test");
-		System.out.println("test2");
+		
 	}
 }
