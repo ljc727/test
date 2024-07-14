122c4e462b6edcd2929fc1df3fd167c93270e8d1
ljc727 <ljc727@naver.com>
2024-07-14 17:34:20 +0900

    714 test1

--- test_web/src/com/ljc/Test.java
+++ test_web/src/com/ljc/Test.java
@@ -2,11 +2,7 @@ package com.ljc;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("2222");
-		System.out.println("11");
-		//test
-		
-		System.out.println();
+
 		
 	}
 }
--- test_web/src/com/ljc/Test1.java
+++ test_web/src/com/ljc/Test1.java
@@ -2,9 +2,7 @@ package com.ljc;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println();
-		// test2
-		//test
+
 	}
 
 }
